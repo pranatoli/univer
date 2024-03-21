4a81bc11c996c2d41bfc5b31e1fa5acf84a4dd6e
pranatoli <pr.anatoli@gmail.com>
2024-03-21 13:39:34 +0300

    start task - univer

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const routes = require('./routes/index')
+
+app.use(express.json())
+app.use('/', routes)
+
+app.listen(port, () => console.log('Server started on port: ' + port))
+console.log('Hello developer')

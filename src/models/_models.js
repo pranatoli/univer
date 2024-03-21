37298365a54cb5544e15efedb4ab64958f0a0272
pranatoli <pr.anatoli@gmail.com>
2024-03-21 14:19:08 +0300

    create models file

---
+++ src/models/_models.js
@@ -0,0 +1,14 @@
+const Students = require('./students')
+const Profile = require('./profile')
+const Teachers = require('./teachers')
+const Courses = require('./courses')
+const Grades = require('./grades')
+const StudentCourses = require('./studentCourses')
+
+module.exports = {
+    Position,
+    Adress,
+    Departament,
+    Worker,
+    DepPositions
+}
---
+++ src/models/courses.js
@@ -0,0 +1,1 @@
+const Sequilize = require('sequelize')

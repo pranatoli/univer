45dbd306a32883398887763418400dbc6383701e
pranatoli <pr.anatoli@gmail.com>
2024-04-22 21:23:23 +0300

    port && run dev

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize(
-    process.env.DATABASE_LOCAL,
-    process.env.DB_USER_LOCAL,
-    process.env.PASSWORD_LOCAL,
-    {
-        host: process.env.HOST_LOCAL,
-        dialect: process.env.DIALECT
-    }
-)
+let db
+
+if (process.env.NODE_ENV == 'DEV') {
+    db = new Sequelize(
+        process.env.DATABASE_LOCAL,
+        process.env.DB_USER_LOCAL,
+        process.env.PASSWORD_LOCAL,
+        {
+            host: process.env.HOST_LOCAL,
+            dialect: process.env.DIALECT
+        }
+    )
+} else {
+    db = new Sequelize(process.env.DATABASE_URL)
+}
 
 module.exports = db
